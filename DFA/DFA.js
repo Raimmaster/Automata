1acc176ab45d6aba571fe67513533d51b6afd3e5
Raim Interiano <rai.interiano@gmail.com>
2017-01-28 22:18:24 -0600

    Initial DFA code: addState, addTransition, modifyTransition, deleteTransition, and deleteState functions have been added for DFA.js; State.js is for State class, which should be used for adding and deleting transitions in a state

---
+++ DFA/DFA.js
@@ -0,0 +1,39 @@
+class DFA{
+  constructor(states = [], alphabet, transitionFunctions = [], startState, acceptanceStates = []){
+    this.states = states;
+    this.alphabet = alphabet;
+    this.transitionFunction = transitionFunction;
+    this.startState = startState;
+    this.finalStates = finalStates;
+  }
+
+  addState(state){
+    this.states.push(state);
+  }
+
+  addTransition(fromState, toState, symbol){
+    let indexOfTransition = this.states.indexOf(fromState);
+    this.states[indexOfTransition].addTransition(symbol, toState);
+  }
+
+  deleteState(state){
+    let indexOfStateToDelete = this.states.indexOf(state);
+    if(indexOfStateToDelete > -1){
+      this.states.splice(indexOfStateToDelete, 1);
+    }
+  }
+
+  deleteTransition(originState, destinyState){
+    let indexOfTransitionToDelete = this.originState.transitions.indexOf(destinyState);
+    if(indexOfTransitionToDelete > -1){
+      this.originState.transitions.splice(indexOfTransitionToDelete, 1);
+    }
+  }
+
+  modifyTransition(originState, destinyState, symbol){
+    let indexOfTransitionToModify = this.originState.transitions.indexOf(destinyState);
+    if(indexOfTransitionToModify > -1){
+      this.originState.transitions[indexOfTransitionToModify] = symbol;
+    }
+  }
+}
---
+++ DFA/State.js
@@ -0,0 +1,10 @@
+class State{
+  constructor(stateName, transitions = []){
+    this.stateName = stateName;
+    this.transitions = transitions;
+  }
+
+  addTransition(symbol, destinyState){
+    
+  }
+}
